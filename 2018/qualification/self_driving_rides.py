b27941c22f9729043296d025416b188bdbe3f8fe
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2018-03-10 16:37:58 +0100

    second improvement

--- 2018/qualification/self_driving_rides.py
+++ 2018/qualification/self_driving_rides.py
@@ -1,6 +1,7 @@
 import sys
 
-class V(object):
+class Vehicle(object):
+    """Vehicle"""
     def __init__(self):
         self.x = 0
         self.y = 0
@@ -13,22 +14,22 @@ class V(object):
 
     def is_busy(self, step):
         if step <= self.last_step:
-#        if step < self.last_step:
             return 1
         return 0
 
     def add_ride(self, step, ride):
-        self.ride_step = abs(ride.x - ride.a) + abs(ride.y - ride.b)
+        self.ride_step = ride.steps
         self.a_step = abs(self.x - ride.a) + abs(self.y - ride.b)
         self.w_step = step + self.a_step
-        if step + self.a_step <= ride.e:
+        if self.w_step <= ride.e:
             self.w_step = ride.e
         self.last_step = self.w_step + self.ride_step
         self.x = ride.x
         self.y = ride.y
         self.rides.append(ride.i)
 
-class R(object):
+class Ride(object):
+    """Ride"""
     def __init__(self, a, b, x, y, e, l):
         self.a = a
         self.b = b
@@ -36,11 +37,11 @@ class R(object):
         self.y = y
         self.e = e
         self.l = l
+        self.steps = abs(x - a) + abs(y - b)
         self.i = 0
 
-class Info(object):
-    """The matrix."""
-
+class Resources(object):
+    """Resources"""
     def __init__(self, rows, cols, v, r, b, s):
         self.rows = rows
         self.cols = cols
@@ -50,21 +51,14 @@ class Info(object):
         self.s = s
         self.rides = []
         self.vehicles = []
-        
-    def __str__(self):
-        """Display the matrix."""
-        return '\n'.join(['<%s>' % ''.join(row) for row in self.rides])
-
-    # def get_score(self):
-    #     return 
 
     def get_ride_index(self, step, v, rides):
-        max_score = 0
-        index = -1
+        max_weight = 0
+        index = 0 # if no ride is assigned
         c = len(rides)
         for i in range(0, c):
             r = rides[i]
-            ride_score = abs(r.x - r.a) + abs(r.y - r.b)
+            ride_score = r.steps
             a_step = abs(v.x - r.a) + abs(v.y - r.b)
             w_step = step + a_step
             bonus_score = 0
@@ -74,22 +68,23 @@ class Info(object):
             if r.l < w_step + ride_score:
                 ride_score = 0
             total_score = bonus_score + ride_score
-            if total_score > max_score:
-                max_score = total_score
+            lost_steps = r.e - step - a_step
+            weight = self.s - a_step
+            if lost_steps > 0 and r.e > 0:
+                weight -= lost_steps
+            if bonus_score > 0:
+                weight += bonus_score
+            if weight > max_weight:
+                max_weight = weight
                 index = i
-
         return index
     
-    def loop(self):
+    def drive(self):
         rides_todo = []
-
         for v in range(0, self.v):
-            self.vehicles.append(V())
-
+            self.vehicles.append(Vehicle())
         for r in range(0, self.r):
-            rides_todo.append(R(*[int(num) for num in self.rides[r].split()]))
-            rides_todo[r].i = r
-            
+            rides_todo.append(self.rides[r])
         for t in range(0, self.s):
             for v in range(0, self.v):
                 if len(rides_todo) > 0:
@@ -97,38 +92,39 @@ class Info(object):
                         ride_index = self.get_ride_index(t, self.vehicles[v], rides_todo)
                         self.vehicles[v].add_ride(t, rides_todo.pop(ride_index))
 
-def read_matrix(filename):
+def read_input(filename):
     """Read the input file."""
     with open(filename, 'r') as fin:
-        _info = Info(*[int(num) for num in fin.readline().split()])
+        resource = Resources(*[int(num) for num in fin.readline().split()])
         # read matrix
-        for i in range(_info.r):
-            str_line = fin.readline().strip()
-            _info.rides.append(str_line)
+        for i in range(resource.r):
+            line = fin.readline().strip()
+            resource.rides.append(Ride(*[int(num) for num in line.split()]))
+            resource.rides[i].i = i
+    return resource
 
-    return _info
-
-def write_matrix(info, filename):
-    """Write output file."""
-    with open(filename, 'w') as out:
-        for v in range(0, info.v):
-            out.write('%d %s\n' % (len(info.vehicles[v].rides), ' '.join([str(x) for x in info.vehicles[v].rides])))
+def write_output(resource, filename):
+    """Write the output file."""
+    print('Output file: %s' % filename)
+    with open(filename, 'w') as fout:
+        for v in range(0, resource.v):
+            fout.write('%d %s\n' % (len(resource.vehicles[v].rides), ' '.join([str(x) for x in resource.vehicles[v].rides])))
 
 def main():
     """Main function."""
 
     if len(sys.argv) < 3:
-        sys.exit('Syntax: %s <filename> <output>' % sys.argv[0])
+        sys.exit('Syntax: %s <input> <output>' % sys.argv[0])
 
     # read data and initialize the matrix
-    info = read_matrix(sys.argv[1])
+    resource = read_input(sys.argv[1])
 
-    info.loop()
+    # self driving rides
+    resource.drive()
 
-#    print('score: %d' % info.get_score())
 
     # write output file
-    write_matrix(info, sys.argv[2])
+    write_output(resource, sys.argv[2])
 
 if __name__ == '__main__':
     main()
